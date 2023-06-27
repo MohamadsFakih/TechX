61e2833712b531392dd8115f67db0184ee12126a
Mohamad Fakih <100450267+MohamadsFakih@users.noreply.github.com>
2023-06-27 22:56:23 +0300

    Added a timer to auto scroll the banners

--- lib/features/home/presentation/screen/home_page.dart
+++ lib/features/home/presentation/screen/home_page.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:techx/core/utils/global_colors.dart';
 import 'package:techx/di/injection_container.dart';
 import 'package:techx/features/home/data/model/item_model.dart';
 import 'package:techx/features/home/presentation/bloc/home_bloc.dart';
@@ -7,6 +11,7 @@ import 'package:techx/features/home/presentation/widgets/list_banners.dart';
 import 'package:techx/features/home/presentation/widgets/list_brands.dart';
 import 'package:techx/features/home/presentation/widgets/home_list_items.dart';
 import 'package:techx/features/home/presentation/widgets/search_bar.dart';
+import 'package:techx/features/login/presentation/screen/login_screen.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -20,6 +25,13 @@ class _HomePageState extends State<HomePage> {
   List<ItemModel> featuredList = [];
   final TextEditingController _searchController = TextEditingController();
   final PageController _pageController = PageController(initialPage: 0);
+
+  int _currentIndex = 0;
+  late final List<Widget> screens = [
+    const LoginPage(
+      key: Key('login'),
+    ),
+  ];
 
   @override
   void initState() {
@@ -46,61 +58,114 @@ class _HomePageState extends State<HomePage> {
       builder: (context, state) {
         return state.isLoading
             ? const Center(child: CircularProgressIndicator())
-            : RefreshIndicator(
-                onRefresh: _refresh,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ListView(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          SizedBox(
-                            width: 46,
-                            height: 46,
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(20),
-                              child: const Image(
-                                image: AssetImage('assets/images/github.png'),
+            : Scaffold(
+                bottomNavigationBar: Material(
+                  elevation: 8,
+                  borderRadius: const BorderRadius.only(
+                      topRight: Radius.circular(16),
+                      topLeft: Radius.circular(16)),
+                  color: GlobalColors.bottomBarColor,
+                  child: BottomNavigationBar(
+                    showSelectedLabels: false,
+                    elevation: 0,
+                    showUnselectedLabels: false,
+                    backgroundColor: Colors.transparent,
+                    type: BottomNavigationBarType.fixed,
+                    unselectedItemColor: GlobalColors.bottomBarItemColor,
+                    selectedItemColor: Colors.white,
+                    currentIndex: _currentIndex,
+                    selectedFontSize: 0,
+                    onTap: (index) => _onBottomTap(index),
+                    items: [
+                      BottomNavigationBarItem(
+                          icon: const Icon(
+                            Icons.home_outlined,
+                          ),
+                          activeIcon: _buildSelectedIcon(Icons.home),
+                          label: ""),
+                      BottomNavigationBarItem(
+                          icon: const Icon(
+                            Icons.shopping_cart,
+                          ),
+                          activeIcon: _buildSelectedIcon(Icons.shopping_cart),
+                          label: ""),
+                      BottomNavigationBarItem(
+                          icon: const Icon(
+                            Icons.search,
+                          ),
+                          activeIcon: _buildSelectedIcon(Icons.search),
+                          label: ""),
+                      BottomNavigationBarItem(
+                          icon: const Icon(
+                            Icons.favorite_border,
+                          ),
+                          activeIcon: _buildSelectedIcon(Icons.favorite),
+                          label: ""),
+                      BottomNavigationBarItem(
+                          icon: const Icon(
+                            Icons.person_outline,
+                          ),
+                          activeIcon: _buildSelectedIcon(Icons.person),
+                          label: ""),
+                    ],
+                  ),
+                ),
+                body: RefreshIndicator(
+                  onRefresh: _refresh,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ListView(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              width: 46,
+                              height: 46,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(20),
+                                child: const Image(
+                                  image: AssetImage('assets/images/github.png'),
+                                ),
                               ),
                             ),
-                          ),
-                          const SizedBox(
-                            width: 4,
-                          ),
-                          const Text("Welcome,\nMohamad"),
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 16,
-                      ),
-                      HomeSearchBar(searchController: _searchController),
-                      const SizedBox(
-                        height: 16,
-                      ),
-                      ListBanners(
-                        pageController: _pageController,
-                        items: state.banners,
-                      ),
-                      const SizedBox(
-                        height: 16,
-                      ),
-                      const ListBrands(),
-                      const SizedBox(
-                        height: 16,
-                      ),
-                      HomeListItems(
-                        items: state.featured,
-                        label: "Featured",
-                      ),
-                      const SizedBox(
-                        height: 16,
-                      ),
-                      HomeListItems(
-                        items: state.newList,
-                        label: "New",
-                      ),
-                    ],
+                            const SizedBox(
+                              width: 4,
+                            ),
+                            const Text("Welcome,\nMohamad"),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 16,
+                        ),
+                        HomeSearchBar(searchController: _searchController),
+                        const SizedBox(
+                          height: 16,
+                        ),
+                        ListBanners(
+                          pageController: _pageController,
+                          items: state.banners,
+                        ),
+                        const SizedBox(
+                          height: 16,
+                        ),
+                        const ListBrands(),
+                        const SizedBox(
+                          height: 16,
+                        ),
+                        HomeListItems(
+                          items: state.featured,
+                          label: "Featured",
+                        ),
+                        const SizedBox(
+                          height: 16,
+                        ),
+                        HomeListItems(
+                          items: state.newList,
+                          label: "New",
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               );
@@ -108,7 +173,34 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  Widget _buildSelectedIcon(IconData icon) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: GlobalColors.redColor,
+        child: Icon(
+          icon,
+          color: Colors.white,
+        ),
+      )
+          .animate()
+          .scale(
+              end: const Offset(
+            1.1,
+            1.1,
+          ))
+          .slideY(end: -.3),
+    );
+  }
+
   Future<void> _refresh() async {
     _bloc.add(const Fetch());
+  }
+
+  _onBottomTap(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
   }
 }
--- lib/features/home/presentation/widgets/list_banners.dart
+++ lib/features/home/presentation/widgets/list_banners.dart
@@ -1,14 +1,36 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:techx/features/home/data/model/item_model.dart';
 import 'package:techx/features/home/presentation/widgets/home_banner.dart';
 
-class ListBanners extends StatelessWidget {
+class ListBanners extends StatefulWidget {
   const ListBanners(
       {super.key, required this.pageController, required this.items});
 
   final PageController pageController;
   final List<BannerItem> items;
+
+  @override
+  State<ListBanners> createState() => _ListBannersState();
+}
+
+class _ListBannersState extends State<ListBanners> {
+  late Timer _timer;
+  int _currentPage = 0;
+
+  @override
+  void initState() {
+    startTime();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,21 +40,39 @@ class ListBanners extends StatelessWidget {
           aspectRatio: 1.7,
           child: PageView.builder(
               scrollDirection: Axis.horizontal,
-              controller: pageController,
-              itemCount: items.length,
+              controller: widget.pageController,
+              itemCount: widget.items.length,
               itemBuilder: (context, pos) {
-                return HomeBanner(image: items[pos].image);
+                _currentPage = pos;
+                _timer.cancel();
+                startTime();
+                return HomeBanner(image: widget.items[pos].image);
               }),
         ),
         const SizedBox(
           height: 8,
         ),
         SmoothPageIndicator(
-          controller: pageController, // PageController
-          count: items.length,
+          controller: widget.pageController, // PageController
+          count: widget.items.length,
           effect: const WormEffect(), // your preferred effect
         ),
       ],
     );
+  }
+
+  void startTime() {
+    _timer = Timer.periodic(const Duration(seconds: 5), (Timer timer) {
+      if (_currentPage < widget.items.length - 1) {
+        _currentPage++;
+      } else {
+        _currentPage = 0;
+      }
+      widget.pageController.animateToPage(
+        _currentPage,
+        duration: const Duration(milliseconds: 350),
+        curve: Curves.easeIn,
+      );
+    });
   }
 }
