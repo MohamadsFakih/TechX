cc6f4dbb5d42408c24ff31103f6fd8486977a019
Mohamad Fakih <100450267+MohamadsFakih@users.noreply.github.com>
2023-07-30 12:07:38 +0300

    Added credit card utils and formatters needed

---
+++ lib/core/components/card_type.dart
@@ -0,0 +1,11 @@
+enum CardType {
+  master,
+  visa,
+  verve,
+  discover,
+  americanExpress,
+  dinersClub,
+  jcb,
+  others,
+  invalid
+}
---
+++ lib/core/components/input_formatters.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/services.dart';
+
+class CardMonthInputFormatter extends TextInputFormatter {
+  @override
+  TextEditingValue formatEditUpdate(
+      TextEditingValue oldValue, TextEditingValue newValue) {
+    var newText = newValue.text;
+
+    if (newValue.selection.baseOffset == 0) {
+      return newValue;
+    }
+
+    var buffer = StringBuffer();
+    for (int i = 0; i < newText.length; i++) {
+      buffer.write(newText[i]);
+      var nonZeroIndex = i + 1;
+      if (nonZeroIndex % 2 == 0 && nonZeroIndex != newText.length) {
+        buffer.write('/');
+      }
+    }
+
+    var string = buffer.toString();
+    return newValue.copyWith(
+        text: string,
+        selection: TextSelection.collapsed(offset: string.length));
+  }
+}
+
+class CardNumberFormatter extends TextInputFormatter {
+  @override
+  TextEditingValue formatEditUpdate(
+      TextEditingValue oldValue, TextEditingValue newValue) {
+    if (newValue.selection.baseOffset == 0) {
+      return newValue;
+    }
+    String inputData = newValue.text;
+    StringBuffer buffer = StringBuffer();
+
+    for (var i = 0; i < inputData.length; i++) {
+      buffer.write(inputData[i]);
+      int index = i + 1;
+
+      if (index % 4 == 0 && inputData.length != index) {
+        buffer.write(" ");
+      }
+    }
+    return TextEditingValue(
+      text: buffer.toString(),
+      selection: TextSelection.collapsed(
+        offset: buffer.toString().length,
+      ),
+    );
+  }
+}
